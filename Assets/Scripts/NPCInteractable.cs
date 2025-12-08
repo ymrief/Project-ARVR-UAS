dac35e36a5a0afa613ec1e91757ddd29c0153f90
Yusuf Martinus Arief <brothanes11@gmail.com>
2025-12-08 13:43:30 +0700

    adding npc every planet and fix minor bug

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
-using TMPro;
 
 public class NPCInteractable : InteractableObject
 {
-    [Header("NPC Dialog")]
-    [TextArea(3, 5)]
-    public string dialogText;
+    [Header("NPC Dialog Data")]
+    [TextArea(3, 10)] // Membuat kotak input di Inspector lebih besar
+    public string[] dialogueLines; // Array String (Bisa isi banyak kalimat)
 
     public override void Interact()
     {
+        // Cek kondisi dasar (jarak/halangan) dari script induk
         if (!isCanInteract()) return;
+        
         base.Interact();
-        ShowDialog();
-        hasInteracted = true;
-    }
 
-    void ShowDialog()
-    {
-        // sementara pakai Debug dulu
-        Debug.Log("NPC berkata: " + dialogText);
+        // Kirim data array ke UI Manager untuk diproses
+        if (UIPlayerManager.Instance != null)
+        {
+            UIPlayerManager.Instance.StartDialogue(dialogueLines);
+        }
+        
+        hasInteracted = true;
     }
 }
